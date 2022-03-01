c5e1a6da0a99a7463f34b6baa09da4696898a9a3
ysYassine <ys.yassine.sellami@gmail.com>
2022-03-01 20:19:16 +0100

    Internationalization

--- MyApp2/webapp/controller/app.controller.js
+++ MyApp2/webapp/controller/app.controller.js
@@ -7,23 +7,31 @@ sap.ui.define(
     //name of the artifact: APP
     return Controller.extend("opensap.myapp.controller.App", {
       onShowHello: function () {
-        MessageToast.show(
-          "Hello World"
-          // {
-          //   duration: 3000, // default
-          //   width: "15em", // default
-          //   my: "center bottom", // default
-          //   at: "center bottom", // default
-          //   of: window, // default
-          //   offset: "0 0", // default
-          //   collision: "fit fit", // default
-          //   onClose: null, // default
-          //   autoClose: true, // default
-          //   animationTimingFunction: "ease", // default
-          //   animationDuration: 1000, // default
-          //   closeOnBrowserNavigation: true, // default
-          // }
-        );
+        // read msg from i18n model
+        let oBundle = this.getView().getModel("i18n").getResourceBundle();
+        let sRecipient = this.getView()
+          .getModel("helloPanel")
+          .getProperty("/recipient/name");
+        let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        // show message
+        MessageToast.show(sMsg);
+        // MessageToast.show(
+        //   "Hello World"
+        //   // {
+        //   //   duration: 3000, // default
+        //   //   width: "15em", // default
+        //   //   my: "center bottom", // default
+        //   //   at: "center bottom", // default
+        //   //   of: window, // default
+        //   //   offset: "0 0", // default
+        //   //   collision: "fit fit", // default
+        //   //   onClose: null, // default
+        //   //   autoClose: true, // default
+        //   //   animationTimingFunction: "ease", // default
+        //   //   animationDuration: 1000, // default
+        //   //   closeOnBrowserNavigation: true, // default
+        //   // }
+        // );
       },
     });
   }
