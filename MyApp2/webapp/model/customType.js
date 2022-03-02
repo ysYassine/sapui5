04eccf28dcb35f15ca30e23c0e46c0b1c05e5bd3
ysYassine <ys.yassine.sellami@gmail.com>
2022-03-02 15:22:43 +0100

    W2_Unit 3 complement

---
+++ MyApp2/webapp/model/customType.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/model/SimpleType"], function (SimpleType) {
+  "use strict";
+  return SimpleType.extend("opensap.myapp.model.customType", {
+    formatValue: function () {},
+    parseValue: function () {},
+    validateValue: function () {},
+  });
+});
