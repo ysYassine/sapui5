9d2547882f6ee2fbb9905fae58dabaf1221b1a65
ysYassine <ys.yassine.sellami@gmail.com>
2022-03-08 15:16:22 +0100

    W2_Unit 2: SimpleForm & Controls

--- 2019Course/week2/webapp/controller/App.controller.js
+++ 2019Course/week2/webapp/controller/App.controller.js
@@ -1,15 +1,28 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast) {
+    "use strict";
 
-        return Controller.extend("week2.controller.App", {
-            onInit: function () {
+    return Controller.extend("week2.controller.App", {
+      onInit: function () {
+        console.log("On init");
+      },
+      onBeforeRendering: function () {
+        console.log("The view will shortly be rendered.");
+      },
+      onAfterRendering: function () {
+        console.log("The view has been rendered.");
+      },
+      onExit: function () {
+        console.log("Controller will shortly be destroyed.");
+      },
 
-            }
-        });
+      onPress: function (oMessage) {
+        MessageToast.show("Searching..." + oMessage);
+      },
     });
+  }
+);
